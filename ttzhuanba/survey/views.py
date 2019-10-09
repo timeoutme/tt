f0024160ec586c660c01fcd8db2628f0899a9110
timeoutme <383480701@qq.com>
2019-10-09 14:13:05 +0800

    获取json数据，到前端显示

--- ttzhuanba/survey/views.py
+++ ttzhuanba/survey/views.py
@@ -1,54 +1,28 @@
 from django.shortcuts import render
 import json
 import urllib.request
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
-
-# 取得问卷数据
-# def fetch_survey(url):
-#     req = request.Request(url)
-#     with request.urlopen(req) as f:
-#         data = f.read()
-#         return json.loads(data.decode('utf-8'))
-
 url = 'http://www.juoffer.com/api/GetQuestions?key=6eda63c007606b16fe5e558ceb6357d1'
-# survey_data = fetch_survey(url)
-# survey_data = survey_data['data']
 
-# 提取问卷数据里面的id, title, link, score
 def survey_list(request):
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as f:
         data = f.read()
     survey_data = json.loads(data.decode('utf-8'))
-    survey_data = survey_data['data']
-    
+    survey_data = survey_data['data']    
     survey_dict = {}
     i = 0
-    for survey in survey_data:
-        # for key, value in survey.items():
-        #     survey_dict = {}
-        #     if key == 'id':
-                
-        #         survey_dict['id'] = value
-                
-        #     elif key == 'title':
-                
-        #         survey_dict['title'] = value
-                
-        #     elif key == 'link':
-                
-        #         survey_dict['link'] = value
-        #     elif key == 'score':
-                
-        #         survey_dict['score'] = value     
-        #         survey_list.append(survey_dict)   
+    for survey in survey_data:       
         survey_dict[i] =  survey
-        i+=1      
-    return render(request,'survey_list.html', {'surveys':survey_dict})   
-#     return survey_dict
-# a = survey_list(request)    
-# print(a)
+        i+=1  
+
+    # 对页面进行分页 
+    survey_paginator = Paginator(survey_dict,15)
+    page_num = request.GET.get('page',1)
+    page_of_surveys = survey_paginator.get_page(page_num) 
+
+    return render(request,'survey_list.html', {'surveys':page_of_surveys})   
